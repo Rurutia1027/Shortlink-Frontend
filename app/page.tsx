69770f4e2b38d82206117b3fe081fb93a7737f43
Rurutia1027 <rurutia1027@gmail.com>
2026-01-19 15:42:27 +0800

    update documents

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation'
-
-/**
- * Root page - redirects to /space (matching Vue: / -> /home -> /home/space)
- */
-export default function Home() {
-  redirect('/space')
-}
