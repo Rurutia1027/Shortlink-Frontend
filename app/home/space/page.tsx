47e0df6078e3b9a1f45896f991af7b5ce22f4386
Rurutia1027 <rurutia1027@gmail.com>
2026-01-19 17:06:34 +0800

    add router diff in vue router and next router, update pages and middleware

---
+++ app/home/space/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+/**
+ * My Space Page
+ * Route: /home/space
+ */
+export default function MySpacePage() {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <p>Short link management will be implemented here</p>
+    </div>
+  )
+}
