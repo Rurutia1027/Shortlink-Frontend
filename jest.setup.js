89125f067611f34a631be713e7e7b6e4232a1183
Rurutia1027 <rurutia1027@gmail.com>
2026-01-21 13:21:50 +0800

    add msw worker and udpate configs for jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,20 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
+import { server } from './mocks/server'
 
 // Extend Jest with visual regression matcher
 expect.extend({ toMatchImageSnapshot })
+
+// Establish API mocking before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }))
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests
+afterEach(() => server.resetHandlers())
+
+// Clean up after the tests are finished
+afterAll(() => server.close())
 
 // Mock Next.js router
 jest.mock('next/navigation', () => ({
