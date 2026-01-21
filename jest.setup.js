559f9c20868d2123f40532d477bbcd553252ce84
Rurutia1027 <rurutia1027@gmail.com>
2026-01-21 13:34:52 +0800

    refine mock server and jest setup config

--- jest.setup.js
+++ jest.setup.js
@@ -1,20 +1,29 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
-import { server } from './mocks/server'
 
 // Extend Jest with visual regression matcher
 expect.extend({ toMatchImageSnapshot })
 
-// Establish API mocking before all tests
-beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }))
-
-// Reset any request handlers that we may add during the tests,
-// so they don't affect other tests
-afterEach(() => server.resetHandlers())
-
-// Clean up after the tests are finished
-afterAll(() => server.close())
+// Setup MSW server for API mocking (only if MSW is installed)
+let server
+try {
+  const mswModule = require('./mocks/server')
+  server = mswModule.server
+  
+  // Establish API mocking before all tests
+  beforeAll(() => server.listen({ onUnhandledRequest: 'warn' }))
+  
+  // Reset any request handlers that we may add during the tests,
+  // so they don't affect other tests
+  afterEach(() => server.resetHandlers())
+  
+  // Clean up after the tests are finished
+  afterAll(() => server.close())
+} catch (error) {
+  // MSW not installed yet - tests will run without API mocking
+  console.warn('MSW not available - API mocking disabled. Run "npm install" to enable.')
+}
 
 // Mock Next.js router
 jest.mock('next/navigation', () => ({
